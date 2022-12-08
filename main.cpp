b4e1b9d259f710f12e84dd4cc20159428eb1381a
jcussen <jcussen@umich.edu>
2022-12-08 02:53:06 -0500

    josh

--- main.cpp
+++ main.cpp
@@ -41,28 +41,104 @@ public:
         }
         return words;
       }
+
     //trains the classifier with the file from filename
-    void train(string filename){
+    void train(string filename, bool debug){
         csvstream csvin(filename);
         map<string, string> map;
+        if (debug) {
+            cout << "training data:" << "\n";
+        }
         while (csvin >> map) {
             const string label = map["tag"];
             const string post = map["content"];
-            cout << "label = " << label << ", content = " << post << endl;
+            insert(label, post);
+            if (debug) {
+                cout << "  label = " << label << ", content = " << post << endl;
+            }
         }
+        
+//        std::map<string, string> map2;
+//        int line_counter = 0;
+//        int vocab_counter = 0;
+//        while(csvin >> map){
+//            line_counter += 1;
+//            cout << map2.size() <<  "\n";
+//            vocab_counter += map2.size();
+//            string label = map2["tag"];
+//            cout << label.size() << "\n";
+//            vocab_counter += label.size();
+//        }
+
+        cout << "trained on " << num_posts << " examples" << "\n";
+        if (debug) {
+            cout << "vocabulary size = " << num_unique_words << "\n";
+        }
+        cout << "\n";
     }
+
     //runs the tests on the test file
+//    test data:
+//      correct = euchre, predicted = euchre, log-probability score = -13.7
+//      content = my code segfaults when bob is the dealer
+//
+//      correct = euchre, predicted = calculator, log-probability score = -12.5
+//      content = no rational explanation for this bug
+//
+//      correct = calculator, predicted = calculator, log-probability score = -13.6
+//      content = countif function in stack class not working
+//
+//    performance: 2 / 3 posts predicted correctly
     void test(string filename){
+        cout << "test data:" << "\n";
+        int posts = 0;
+        int correct = 0;
         csvstream csvin(filename);
         map<string, string> map;
         while (csvin >> map) {
             const string &post = map["content"];
-            // const string &label = map["tag"];
-            set<string> unique_word = unique_words(post);
-            //call log_prob and those functions here
-            cout << most_likely_label(post) << "\n";
+            set<string> bag = make_bag_of_words(post);
+            const string &label = map["tag"];
+            cout << "  correct = " << label << ", predicted = ";
+            cout << most_likely_label(post) << ", log-probability score = ";
+            cout << log_prob(bag, most_likely_label(post)) << endl;
+            cout << "  content = " << post << endl << endl;
+            posts++;
+            if (label == most_likely_label(post)) {
+                correct++;
+            }
         }
+        cout << "performance: " << correct << " / " << posts;
+        cout << " posts predicted correctly" << endl;
       }
+    
+//classes:
+//  calculator, 3 examples, log-prior = -0.981
+//  euchre, 5 examples, log-prior = -0.47
+//   classifier parameters:
+//   calculator:assert, count = 1, log-likelihood = -1.1
+    void debug(bool d) {
+        if (d) {
+            cout << "classes:" << endl;
+            for (auto &p : labels) {
+                cout << "  " << p.first << ", " << p.second.num_posts;
+                cout << " examples, log-prior = " << log_prob(p.first) << endl;
+            }
+            cout << "classifier parameters:" << endl;
+            for (auto &p : labels) {
+                for (auto &q : p.second.words) {
+                    cout << "  " << p.first << ":" << q.first << ", count = ";
+                    cout << q.second << ", log-likelihood = ";
+                    cout << log_prob(p.first, q.first) << endl;
+                }
+            }
+            cout << endl;
+        }
+        else {
+            return;
+        }
+    }
+    
     //inserts a new post into the classifier
     void insert(const string label, const string post) {
         set<string> bag = make_bag_of_words(post);
@@ -96,24 +172,28 @@ public:
         return sum;
     }
     
-    //finds the log probability of finding a post with the given label
+    //finds the log probability of finding a post
+    //with the given label
     //see formula on the spec
     // word is read in map in training data
     // word wasnt seen with label, but was seen in training data
     // No word was seen at all
     double log_prob(string label){
     //get log prob of that label and content
-    //go through all the labels in the training and see which ones have the highest probability score
+    //go through all the labels in the training and
+    //see which ones have the highest probability score
         double np = static_cast<double>(num_posts);
-        return log(static_cast<double>(num_posts_label(label))/np);
+        double npl = static_cast<double>(num_posts_label(label));
+        return log(npl/np);
     }
     
-    //finds the log probability of finding a post with the given word based on the given label
+    //finds the log probability of finding a post with the given word
+    //based on the given label
     //see formula on the spec
     double log_prob(string label, string word) {
         double np = static_cast<double>(num_posts);
         if (allwords.find(word) == allwords.end()){
-            return log(1/np);
+            return log(1.0/np);
         }
         else if (labels[label].words.find(word) == labels[label].words.end()) {
             return log(static_cast<double>(num_posts_word(word))/np);
@@ -150,14 +230,14 @@ private:
 //      -For each word, the number of posts in the entire training set that contain .
 //      -For each label, the number of posts with that label.
 //      -For each label and word, the number of posts with label that contain that word.
-    
     //Label which stores a map of all the words in that label
     //and the number of posts they appear in, as well as the
     //total number of posts in the label
     class Label {
         friend Classifier;
     private:
-        //map of all words under that label, with the number of posts that contain that word
+        //map of all words under that label, with the
+        //number of posts that contain that word
         map<string,int> words;
         int num_posts;
         
@@ -203,89 +283,95 @@ private:
     int num_unique_words;   //number of unique words across all posts
 };
 
-// void test();
+void tests();
 int main (int argc, char * argv[]) {
     cout.precision(3);
+    
+//    cout << "enter: 0 for test suite, 1 for standard main funciton" << endl;
+//    int input;
+//    cin >> input;
+//    if (input != 0 && input != 1) {
+//        cout << "bruh" << endl << endl;
+//        return -1;
+//    }
+//    else if (input == 0) {
+//        tests();
+//        return 0;
+//    }
+    
+    
+    bool debug = false;
     if(!(argc == 3 || argc == 4)) {
         cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
         return -1;
     }
-    
-    if (argc == 4 && !strcmp(argv[3],"[--debug]")) {
-        cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
-        return -1;
-    }
-    
-    else {
-        ifstream infile;
-        ifstream infile2;
-        infile.open(argv[1]);
-        infile2.open(argv[2]);
-        cout << "enter: 0 for test suite, 1 for standard main funciton" << endl;
-        int input;
-        cin >> input;
-        if (input != 0 && input != 1) {
-            cout << "bruh" << endl;
+
+    if (argc == 4) {
+        if(!strcmp(argv[3],"[--debug]")) {
+            cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
             return -1;
         }
-        else if (input == 0) {
-            // test();                               
-            return 0;
+        else {
+            debug = true;
         }
-        else if(!infile.is_open()){
-            cout << "Error opening file: " << argv[1] << endl;
-        }
-
-        else if(!infile2.is_open()){
-            cout << "Error opening file: " << argv[2] << endl;
-        }
-
-        else{
-            string train_file = argv[1];
-            string test_file = argv[2];
-            Classifier classifier;
-            
-            cout << "training data:" << "\n";
-            classifier.train(train_file);
-            csvstream csvin(train_file);
-            map<string, string> map;
-            int line_counter = 0;
-            int vocab_counter = 0;
-            while(csvin >> map){
-                line_counter += 1;
-                cout << map.size() <<  "\n";
-                vocab_counter += map.size();
-                string label = map["tag"];
-                cout << label.size() << "\n";
-                vocab_counter += label.size();
-            }
-
-            cout << "Trained on " << line_counter << " examples" << "\n";
-            cout << "Vocabulary Size = " << vocab_counter << "\n";
-
-            cout << "test data: " << "\n";
-            classifier.test(test_file);
-            return 0;
-         }  
-
     }
+    ifstream infile;
+    ifstream infile2;
+    infile.open(argv[1]);
+    infile2.open(argv[2]);
+    
+    if(!infile.is_open()){
+        cout << "Error opening file: " << argv[1] << endl;
+    }
+    
+    else if(!infile2.is_open()){
+        cout << "Error opening file: " << argv[2] << endl;
+    }
+    
+    string train_file = argv[1];
+    string test_file = argv[2];
+    Classifier classifier;
+    classifier.train(train_file,debug);
+    classifier.debug(debug);
+    classifier.test(test_file);
+    return 0;
 }
 
-// GO TO LINE 229 AND 207 AND REMOVE COMMENT WHEN TEST
-// void test() {
-//     cout << "testing..." << endl;
-//     Classifier classifier;
-//     classifier.insert("euchre", "the left bower took the trick");
-//     classifier.insert("euchre", "the right bower took the trick");
-//     classifier.insert("calculator", "how do you add the first one");
-//     assert(classifier.num_posts_word("the") == 3);
-//     assert(classifier.num_posts_label("euchre") == 2);
-//     assert(classifier.num_posts_label_and_word("euchre", "trick") == 2);
-//     assert(classifier.num_posts_label_and_word("euchre", "left") == 1);
-//     assert(classifier.most_likely_label("left bower") == "euchre");
-//     assert(classifier.most_likely_label("add") == "calculator");
-//     assert(classifier.most_likely_label("the") == "euchre");
-//     cout << "test passed!!!!!!" << endl;
-// }
+ //GO TO LINE 229 AND 207 AND REMOVE COMMENT WHEN TEST
+ void tests() {
+     cout << "testing. . ." << endl;
+     Classifier classifier;
+     classifier.insert("euchre", "the left bower took the trick");
+     classifier.insert("euchre", "the right bower took the trick");
+     classifier.insert("calculator", "how do you add the first one");
+     assert(classifier.num_posts_word("the") == 3);
+     assert(classifier.num_posts_label("euchre") == 2);
+     assert(classifier.num_posts_label_and_word("euchre", "trick") == 2);
+     assert(classifier.num_posts_label_and_word("euchre", "left") == 1);
+     assert(classifier.most_likely_label("left bower") == "euchre");
+     assert(classifier.most_likely_label("add") == "calculator");
+     assert(classifier.most_likely_label("the") == "euchre");
+     
+//    classifier.insert("euchre","can the upcard ever be the left bower");
+//    classifier.insert("euchre","when would the dealer ever prefer a card \
+     to the upcard");
+//    classifier.insert("euchre","bob played the same card twice is he cheating");
+//    classifier.insert("euchre","how to remove a card from players hand");
+//    classifier.insert("euchre","anyone want to play some euchre");
+//    classifier.insert("calculator","how to assert rational invariants");
+//    classifier.insert("calculator","does stack need its own big three");
+//    classifier.insert("calculator","valgrind memory error not sure what it means");
+//     set<string> bag = classifier.make_bag_of_words("my code segfaults when\
+     bob is the dealer");
+//     cout << classifier.log_prob(bag, "euchre") << endl;
+//     bag = classifier.make_bag_of_words("no rational explanation for this bug");
+//     cout << classifier.log_prob(bag, "calculator") << endl;
+//     bag = classifier.make_bag_of_words("countif function in stack class not \
+     working");
+//     cout << classifier.log_prob(bag, "calculator") << endl;
+     
+     
+     cout << "test passed!!!!!!" << endl << endl;
+ }
 
 
